2c6395a2e8fadee418f240cbd51aba50fb0996b1
Elmahdi Mahmoud <elmahdimahmoud@ap-elmahdim.local>
2020-09-24 15:30:14 +0300

    Update meta tags

--- src/App.js
+++ src/App.js
@@ -14,9 +14,21 @@ function App() {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>Posts</title>
-        <meta name="description" content="Posts page description" />
-        <meta property="og:image" content="https://images.unsplash.com/photo-1472289065668-ce650ac443d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"></meta>
+        <title>Total Posts - {posts.length}</title>
+        <meta name="title" content={`Total Posts - ${posts.length}`} />
+        <meta name="description" content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://react-spa-seo.netlify.app/" />
+        <meta property="og:title" content={`Total Posts - ${posts.length}`} />
+        <meta property="og:description" content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
+        <meta property="og:image" content="https://images.unsplash.com/photo-1472289065668-ce650ac443d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://react-spa-seo.netlify.app/" />
+        <meta property="twitter:title" content={`Total Posts - ${posts.length}`} />
+        <meta property="twitter:description" content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
+        <meta property="twitter:image" content="https://images.unsplash.com/photo-1472289065668-ce650ac443d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
       </Helmet>
       <h1>{posts.length} items</h1>
       <ul>
