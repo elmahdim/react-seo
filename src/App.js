efe17e2de1ffd31be91848befa56bc324adf0eab
Elmahdi Mahmoud <elmahdimahmoud@ap-elmahdim.local>
2020-09-24 15:10:40 +0300

    Add react-helmet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
 
 function App() {
   const [posts, getPosts] = useState([]);
@@ -11,6 +12,12 @@ function App() {
 
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Posts</title>
+        <meta name="description" content="Posts page description" />
+        <meta property="og:image" content="https://images.unsplash.com/photo-1472289065668-ce650ac443d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"></meta>
+      </Helmet>
       <h1>{posts.length} items</h1>
       <ul>
         {posts.map((post) => (
